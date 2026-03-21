e794ba8ebf98f4cf18b0d70fc388d13350343220
枫沐x <98091424+xmmtx@users.noreply.github.com>
2026-03-21 10:41:03 +0800

    Add descriptions to language enum values

--- YoutubeDownloader/Localization/Language.cs
+++ YoutubeDownloader/Localization/Language.cs
@@ -1,12 +1,27 @@
+using System.ComponentModel;
+
 namespace YoutubeDownloader.Localization;
 
 public enum Language
 {
+    [Description("System Default")]
     System,
-    Chinese(Simplified),
+    
+    [Description("Chinese (Simplified)")]
+    ChineseSimplified,
+    
+    [Description("English")]
     English,
+    
+    [Description("Français")]
     French,
+    
+    [Description("Deutsch")]
     German,
+    
+    [Description("Español")]
     Spanish,
+    
+    [Description("Українська")]
     Ukrainian,
 }
